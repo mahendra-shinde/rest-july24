90ead8c4980e84146a360a93797986074c7ad1ff
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-07-26 16:09:53 +0530

    Enable CORS to allow clients using Any Domain

--- demo-1/src/main/java/com/mahendra/demo1/Main.java
+++ demo-1/src/main/java/com/mahendra/demo1/Main.java
@@ -23,7 +23,7 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.mahendra.demo1 package
-        final ResourceConfig rc = new ResourceConfig().register(MoxyXmlFeature.class).packages("com.mahendra.demo1");
+        final ResourceConfig rc = new ResourceConfig().register(CORSProvider.class).packages("com.mahendra.demo1");
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
